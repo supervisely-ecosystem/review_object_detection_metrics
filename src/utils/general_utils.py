0673c26959a438b8d872f37ae69190fc14e01518
max kolomeychenko <maxim.kolomeychenko@gmail.com>
2021-07-21 16:23:34 +0300

    configure debug + reqs - wip

--- src/utils/general_utils.py
+++ src/utils/general_utils.py
@@ -4,7 +4,7 @@ import os
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
-from PyQt5 import QtCore, QtGui
+#from PyQt5 import QtCore, QtGui
 from src.utils.enumerators import BBFormat
 
 
@@ -142,21 +142,23 @@ def remove_file_extension(filename):
 
 
 def image_to_pixmap(image):
-    image = image.astype(np.uint8)
-    if image.shape[2] == 4:
-        qformat = QtGui.QImage.Format_RGBA8888
-    else:
-        qformat = QtGui.QImage.Format_RGB888
-
-    image = QtGui.QImage(image.data, image.shape[1], image.shape[0], image.strides[0], qformat)
-    # image= image.rgbSwapped()
-    return QtGui.QPixmap(image)
+    raise NotImplementedError()
+    # image = image.astype(np.uint8)
+    # if image.shape[2] == 4:
+    #     qformat = QtGui.QImage.Format_RGBA8888
+    # else:
+    #     qformat = QtGui.QImage.Format_RGB888
+    #
+    # image = QtGui.QImage(image.data, image.shape[1], image.shape[0], image.strides[0], qformat)
+    # # image= image.rgbSwapped()
+    # return QtGui.QPixmap(image)
 
 
 def show_image_in_qt_component(image, label_component):
-    pix = image_to_pixmap((image).astype(np.uint8))
-    label_component.setPixmap(pix)
-    label_component.setAlignment(QtCore.Qt.AlignCenter)
+    raise NotImplementedError()
+    # pix = image_to_pixmap((image).astype(np.uint8))
+    # label_component.setPixmap(pix)
+    # label_component.setAlignment(QtCore.Qt.AlignCenter)
 
 
 def get_files_recursively(directory, extension="*"):
